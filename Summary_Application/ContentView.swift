75188d90ebfafb32d061225d825710c5d57a0ab8
Rentaro Suzuki <rentajaxa96@gmail.com>
2025-05-25 10:38:18 +0900

    Initial Commit

---
+++ Summary_Application/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Summary_Application
+//
+//  Created by 鈴木廉太郎 on 2025/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Summary_Application/Summary_ApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Summary_ApplicationApp.swift
+//  Summary_Application
+//
+//  Created by 鈴木廉太郎 on 2025/05/25.
+//
+
+import SwiftUI
+
+@main
+struct Summary_ApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
